d95641082f52f32da756804e250e7a8bbffd8b1a
wgolyakov <wolodia@narod.ru>
2024-08-28 13:18:41 +0300

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,33 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+	fun fuel(mass: Int): Int {
+		val f = mass / 3 - 2
+		if (f <= 0) return 0
+		return f
+	}
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+	fun fuel2(mass: Int): Int {
+		var m = mass
+		var sum = 0
+		while (m > 0) {
+			m = fuel(m)
+			sum += m
+		}
+		return sum
+	}
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+	fun part1(input: List<String>): Int {
+		return input.map { it.toInt() }.sumOf { fuel(it) }
+	}
 
-    val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+	fun part2(input: List<String>): Int {
+		return input.map { it.toInt() }.sumOf { fuel2(it) }
+	}
+
+	val testInput = readInput("Day01_test")
+	check(part1(testInput) == 2 + 2 + 654 + 33583)
+	check(part2(testInput) == 2 + 2 + 966 + 50346)
+
+	val input = readInput("Day01")
+	part1(input).println()
+	part2(input).println()
 }
